bd1b79c8249600dd9bd6c13617e9ad6dd9cd3047
3ngineHouse <79860022+3ngineHouse@users.noreply.github.com>
2024-12-07 23:07:26 +0000

    day 4 started

---
+++ src/java/Day04.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Day04 {
+    public static void main(String[] args) throws IOException {
+        Path filePath = Paths.get("src/resources/Day04.txt");
+        
+    }
+}
